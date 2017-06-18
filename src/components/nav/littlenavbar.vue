8968faf50230d0476b16832a1547b38d150860f3
Paul Gowder <paul.gowder@gmail.com>
2017-06-18 10:56:54 -0500

    work on littlenav (needs lots more work)

--- src/components/nav/littlenavbar.vue
+++ src/components/nav/littlenavbar.vue
@@ -2,16 +2,17 @@
 
 <div class="littlenavbox">
  <littleicon icon="home" word="home" toggle="navigation" class="homeicon littlenavicon"></littleicon>
- <littleicon icon="user-secret" word="bio" toggle="bio" class="littlenavicon"></littleicon>
- <littleicon icon="flask" word="research" toggle="pubs" class="littlenavicon"></littleicon>
- <littleicon icon="list-ul" word="cv" toggle="cv" class="littlenavicon"></littleicon>
+
+<littleicon icon="user-secret" word="bio" toggle="bio" class="littlenavicon"></littleicon>
+
+<littleicon icon="flask" word="research" toggle="pubs" class="littlenavicon"></littleicon>
+
+<littleicon icon="list-ul" word="cv" toggle="cv" class="littlenavicon"></littleicon>
  
-<littleicon icon="pencil" word="writing" toggle="writing" class="littlenavicon"></littleicon>
-<littleicon icon="lightbulb" word="teaching" toggle="teaching" class="littlenavicon"></littleicon>
+<littleicon icon="pencil" word="external" toggle="writing" class="littlenavicon"></littleicon>
+
 <littleicon icon="code" word="tech" toggle="tech" class="littlenavicon"></littleicon>
 
-<littleicon icon="news" word="news" toggle="news" class="littlenavicon"></littleicon>
-<littleicon icon="rocket" word="links" toggle="links" class="littlenavicon"></littleicon>
 <littleicon icon="address-card" word="contact" toggle="contact" class="littlenavicon"></littleicon>
 
 </div>
