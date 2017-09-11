3df824bed843978e0c56e1de79baa2d166f34836
Paul Gowder <paul.gowder@gmail.com>
2017-09-11 08:28:46 -0500

    experimental hash url

--- src/mixins.js
+++ src/mixins.js
@@ -2,7 +2,8 @@ var nav = {
     props: ["icon", "word", "toggle"],
     methods: {
 	      navto: function(){
-            window.history.pushState({sentTo: this.toggle}, "", this.word);
+            var hashurl = "#" + this.word;
+            window.history.pushState({sentTo: this.toggle}, "", hashurl);
             this.$store.commit('navigate', this.toggle);
                          },
         iconPath: function(nom){return require("./assets/icons/" + nom + ".svg")} // see "getting asset path in javascript" in docs here. http://vuejs-templates.github.io/webpack/static.html  If this doesn't work right, I can always switch to just requiring them in state.js and getting the URLs from the store with vuex.
@@ -13,3 +14,5 @@ var nav = {
 };
 
 export default {nav}
+
+// I should be able to direct users to subpages with hashurls via https://stackoverflow.com/questions/7814725/is-it-possible-to-call-javascript-from-an-anchor-tag 
