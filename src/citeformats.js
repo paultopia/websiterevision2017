750d6977dabdd436a992f831b663d117c0605b67
Paul Gowder <paultopia@users.noreply.github.com>
2018-02-08 19:16:22 -0600

    start refactor to fix horrific citation format spaghetti code

---
+++ src/citeformats.js
@@ -0,0 +1,42 @@
+const italicizer (text) => `*${text}*`;
+const quoter (text) => `"${text}"`;
+
+function chicago(p){
+	var text = "";
+	var markdown = "";
+	const author = p.coauthor? p.coauthor.split(" ").reverse().join(", ") + ", and Paul Gowder." : "Gowder, Paul";
+	const numbers = p.volume ? `${p.volume}${p.issue ? `.${p.issue}` : ""} (${p.year})	:${p.firstpage}-${p.lastpage}` : "";
+	switch (p.type) {
+	case "peer review":
+		text = `${author}. ${quoter(`${p.title}.`)} ${p.journal} ${numbers}.`
+		markdown = `${author}. ${quoter(`${p.title}.`)} ${italicizer(p.journal)} ${numbers}.`
+		break;
+	case "law review":
+		text = `${author}. ${quoter(`${p.title}.`)} ${p.journal} ${numbers}.`
+		markdown = `${author}. ${quoter(`${p.title}.`)} ${italicizer(p.journal)} ${numbers}.`
+		break;
+	case "book":
+		text = `${author}. ${p.title}. ${p.address}: ${p.publisher}, ${p.year}.`;
+		markdown = `${author}. ${italicizer(p.title)}. ${p.address}: ${p.publisher}, ${p.year}.`;
+		break;
+	case "chapter":
+		text = `${author}. ${quoter(`${p.title}.`)} In ${p.book}, ed. ${p.editor}, ${p.firstpage}-${p.lastpage}. ${p.address}: ${p.publisher}, ${p.year}.`;
+		markdown = `${author}. ${quoter(`${p.title}.`)} In ${italicizer(p.book)}, ed. ${p.editor}, ${p.firstpage}-${p.lastpage}. ${p.address}: ${p.publisher}, ${p.year}.`;
+		break;
+	default:
+		console.log("no citation for this type")
+	}
+	return {text, markdown}
+}
+
+
+
+
+
+
+
+function formatCite(inpub, format){
+	markdown = ""
+	text = ""
+	return {"markdown": markdown, "text": text}}
+	
