26d18c80f7aef4d7a388d985c8faedab26b0d334
Paul Gowder <paul.gowder@gmail.com>
2018-02-09 14:31:06 -0600

    add bluebook to cite refactor

--- src/citeformats.js
+++ src/citeformats.js
@@ -1,5 +1,3 @@
-// import bbabbrv from './assets/json/bbtables.json';
-
 const italicizer = (text) => `<i>${text}</i>`;
 const quoter = (text) => `"${text}"`;
 const smallcapser = (text) => `<span style="font-variant:small-caps;">${text}</span>`;
@@ -14,39 +12,51 @@ function bbjournal(journal, bb) {
 
 
 function chicago(p, _){
-	var text = "";
-	var html = "";
-	const author = p.coauthor? p.coauthor.split(" ").reverse().join(", ") + ", and Paul Gowder." : "Gowder, Paul";
-	const numbers = p.volume ? `${p.volume}${p.issue ? `.${p.issue}` : ""} (${p.year})	:${p.firstpage}-${p.lastpage}` : "";
-	switch (p.type) {
-	case "peer review":
-		text = `${author}. ${quoter(`${p.title}.`)} ${p.journal} ${numbers}.`
-		html = `${author}. ${quoter(`${p.title}.`)} ${italicizer(p.journal)} ${numbers}.`
-		break;
-	case "law review":
-		text = `${author}. ${quoter(`${p.title}.`)} ${p.journal} ${numbers}.`
-		html = `${author}. ${quoter(`${p.title}.`)} ${italicizer(p.journal)} ${numbers}.`
-		break;
-	case "book":
-		text = `${author}. ${p.title}. ${p.address}: ${p.publisher}, ${p.year}.`;
-		html = `${author}. ${italicizer(p.title)}. ${p.address}: ${p.publisher}, ${p.year}.`;
-		break;
-	case "chapter":
-		text = `${author}. ${quoter(`${p.title}.`)} In ${p.book}, ed. ${p.editor}, ${p.firstpage}-${p.lastpage}. ${p.address}: ${p.publisher}, ${p.year}.`;
-		html = `${author}. ${quoter(`${p.title}.`)} In ${italicizer(p.book)}, ed. ${p.editor}, ${p.firstpage}-${p.lastpage}. ${p.address}: ${p.publisher}, ${p.year}.`;
-		break;
-	default:
-		console.log("no citation for this type")
-	}
-	return {text, html}
+	  var text = "";
+	  var html = "";
+	  const author = p.coauthor? p.coauthor.split(" ").reverse().join(", ") + ", and Paul Gowder." : "Gowder, Paul";
+	  const numbers = p.volume ? `${p.volume}${p.issue ? `.${p.issue}` : ""} (${p.year})	:${p.firstpage}-${p.lastpage}` : "";
+	  switch (p.type) {
+	  case "peer review":
+        // horrible fallthrough fuckery I may regret this oh god.  I don't want to duplicate journal code though!
+	  case "law review":
+		    text = `${author}. ${quoter(`${p.title}.`)} ${p.journal} ${numbers}.`
+		    html = `${author}. ${quoter(`${p.title}.`)} ${italicizer(p.journal)} ${numbers}.`
+		    break;
+	  case "book":
+		    text = `${author}. ${p.title}. ${p.address}: ${p.publisher}, ${p.year}.`;
+		    html = `${author}. ${italicizer(p.title)}. ${p.address}: ${p.publisher}, ${p.year}.`;
+		    break;
+	  case "chapter":
+		    text = `${author}. ${quoter(`${p.title}.`)} In ${p.book}, ed. ${p.editor}, ${p.firstpage}-${p.lastpage}. ${p.address}: ${p.publisher}, ${p.year}.`;
+		    html = `${author}. ${quoter(`${p.title}.`)} In ${italicizer(p.book)}, ed. ${p.editor}, ${p.firstpage}-${p.lastpage}. ${p.address}: ${p.publisher}, ${p.year}.`;
+		    break;
+	  default:
+		    console.log("no citation for this type");
+	  }
+	  return {text, html};
 }
 
 
-function bluebook (p){
-	var text = "";
-	var html = "";
-	return {text, html}
-	}
+function bluebook (p, bb){
+	  var text = "";
+	  var html = "";
+    const author = p.coauthor ? `${p.coauthor} & Paul Gowder` : "Paul Gowder";
+    const journal = p.journal ? bbjournal(p.journal, bb) : "";
+    switch (p.type) {
+    case "law review":
+    case "peer review":
+        text = `${author}, ${p.title}, ${p.volume} ${journal} ${p.firstpage} (${p.year}).`;
+        html = `${author}, ${italicizer(p.title)}, ${p.volume} ${smallcapser(journal)} ${p.firstpage} (${p.year}).`;
+    case "book":
+        text = `${author}, ${p.title}, (${p.year}).`;
+        html = `${author}, ${italicizer(p.title)}, (${p.year}).`;
+    case "chapter":
+        text = `${author}, ${`${p.title}`}, in ${p.book} ${p.firstpage}-${p.lastpage} (${p.editor} ed. ${p.year}).`;
+        html = `${author}, ${italicizer(`${p.title}`)}, in ${smallcapser(p.book)} ${p.firstpage}-${p.lastpage} (${p.editor} ed. ${p.year}).`;
+    }
+	  return {text, html};
+}
 
 const styleMapper = {chicago, bluebook}
 
