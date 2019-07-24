f8a8ff84aef19d7106548626efce5254302d5858
Paul Gowder <paultopia@users.noreply.github.com>
2019-07-24 15:51:12 +0100

    hyper aggressive linting caught another bug

--- src/ris.js
+++ src/ris.js
@@ -1,77 +1,124 @@
-import { get } from "./utils.js";
+import {get} from "./utils.js";
 
-const jsonRisMapping = {"abstract":"AB",
-                        "address":"CY",
-                        "book":"T2",
-                        "editor":"ED",
-                        "firstpage":"SP",
-                        "issue":"IS",
-                        "journal":"JF",
-                        "lastpage":"EP",
-                        "publink":"UR",
-                        "publisher":"PB",
-                        "title":"TI",
-                        "volume":"VL",
-                        "year":"PY"};
+const jsonRisMapping = {"abstract": "AB",
+    "address": "CY",
+    "book": "T2",
+    "editor": "ED",
+    "firstpage": "SP",
+    "issue": "IS",
+    "journal": "JF",
+    "lastpage": "EP",
+    "publink": "UR",
+    "publisher": "PB",
+    "title": "TI",
+    "volume": "VL",
+    "year": "PY"};
 
-function compositor(pubitem, funcs){
-	return funcs.map(function(func){
-		return func(pubitem);
-	}).join(",\n") + "\nER  - \n";
+function compositor (pubitem, funcs) {
+
+    return `${funcs.map((func) => func(pubitem)).join(",\n")}\nER  - \n`;
+
 }
 
-function basicR(entry){
-  return pubitem => jsonRisMapping[entry] + "  - " + pubitem[entry];
+function basicR (entry) {
+
+    return (pubitem) => `${jsonRisMapping[entry]}  - ${pubitem[entry]}`;
+
 }
 
-function typeMaker(pubitem){
-	var refType = {"law review": "JOUR", "peer review": "JOUR", 
-		"chapter": "CHAP", "book": "BOOK"}[pubitem.type]; 
-	return "\nTY  - " + refType;
+function typeMaker (pubitem) {
+
+    const refType = {"law review": "JOUR",
+        "peer review": "JOUR",
+        "chapter": "CHAP",
+        "book": "BOOK"}[pubitem.type];
+
+    return `\nTY  - ${refType}`;
+
 }
 
-function authorFlip(author){
-  // won't work with jr etc.
-  var alist = author.split(" ");
-  var lastname = author.pop();
-  return "AU  - " + lastname + "," + alist.join(",");
+function authorFlip (author) {
+
+    // Won't work with jr etc.
+    var alist = author.split(" ")
+    const lastname = alist.pop();
+
+    return `AU  - ${lastname},${alist.join(",")}`;
+
 }
 
-function authorMaker(pubitem){
- const moi = "AU  - Gowder,Paul";
- return pubitem.coauthor ? authorFlip(pubitem.coauthor) + "\n" + moi : moi;
+function authorMaker (pubitem) {
+
+    const moi = "AU  - Gowder,Paul";
+
+    return pubitem.coauthor ? `${authorFlip(pubitem.coauthor)}\n${moi}` : moi;
+
 }
 
-function yearMaker(pubitem){
-  return "PY  - " + pubitem.year + "///";
+function yearMaker (pubitem) {
+
+    return `PY  - ${pubitem.year}///`;
+
 }
 
-const funcUp = fields => [typeMaker, authorMaker, yearMaker].concat(fields.map(basicR));
+const funcUp = (fields) => [
+    typeMaker,
+    authorMaker,
+    yearMaker
+].concat(fields.map(basicR));
 
-function articleRis(pubitem){
-  const fields = ["title", "journal", "volume", "firstpage", "lastpage"];
-  const baseLineFuncs = funcUp(fields);
-  const lineFuncs = pubitem.issue ? baseLineFuncs.concat([basicR("issue")]) : baseLineFuncs;
-  return compositor(pubitem, lineFuncs);
+function articleRis (pubitem) {
+
+    const fields = [
+            "title",
+            "journal",
+            "volume",
+            "firstpage",
+            "lastpage"
+        ],
+        baseLineFuncs = funcUp(fields),
+        lineFuncs = pubitem.issue ? baseLineFuncs.concat([basicR("issue")]) : baseLineFuncs;
+
+    return compositor(pubitem, lineFuncs);
+
 }
 
-function chapterRis(pubitem){
-  const fields = ["title", "editor", "book", "firstpage", "lastpage", "publisher", "address"];
-  return compositor(pubitem, funcUp(fields));
+function chapterRis (pubitem) {
+
+    const fields = [
+        "title",
+        "editor",
+        "book",
+        "firstpage",
+        "lastpage",
+        "publisher",
+        "address"
+    ];
+
+    return compositor(pubitem, funcUp(fields));
+
 }
 
-function bookRis(pubitem){
-  const fields = ["title", "address", "publisher"];
-  return compositor(pubitem, funcUp(fields))
+function bookRis (pubitem) {
+
+    const fields = [
+        "title",
+        "address",
+        "publisher"
+    ];
+
+    return compositor(pubitem, funcUp(fields));
+
 }
 
-function makeRis(pubs){
-	return pubs.map(function(pubitem){
-      return get({"law review": articleRis,
-                  "peer review": articleRis,
-                  "chapter": chapterRis,
-                  "book": bookRis}, pubitem.type, _=>null)(pubitem);
-	}).filter(elem=>elem).join("\n");
+function makeRis (pubs) {
+
+    return pubs.map((pubitem) => get({"law review": articleRis,
+      	"peer review": articleRis,
+      	"chapter": chapterRis,
+      	"book": bookRis}, pubitem.type, (_) => null)(pubitem)).filter((elem) => elem).
+        join("\n");
+
 }
 
 export default makeRis;
