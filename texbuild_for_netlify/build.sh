16125226a595703cfb739f2a797b11a153993c73
Paul Gowder <paultopia@users.noreply.github.com>
2020-02-15 18:54:20 -0600

    try rouble installing moderncv to get its packages that way

--- texbuild_for_netlify/build.sh
+++ texbuild_for_netlify/build.sh
@@ -78,6 +78,7 @@ tlmgr install etoolbox
 tlmgr install xcolor
 tlmgr install microtype
 tlmgr install l3kernel
+tlmgr install moderncv
 
 python "$TEXLIVEONFLY" -c latexmk -a "-g -pdf -synctex=1 -interaction=nonstopmode" "$@"
 
