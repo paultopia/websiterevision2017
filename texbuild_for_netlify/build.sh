68ddeb3d87b503f1c59f6b50e4be3b38c6f76a05
Paul Gowder <paul.gowder@gmail.com>
2021-04-21 12:01:22 -0500

    attempting a minimal cloud install

--- texbuild_for_netlify/build.sh
+++ texbuild_for_netlify/build.sh
@@ -21,14 +21,25 @@ fi
 NETLIFY_CACHE_DIR="$NETLIFY_BUILD_BASE/cache"
 
 TEXLIVE_DIR="$NETLIFY_CACHE_DIR/texlive"
-TEXLIVE_BIN="$TEXLIVE_DIR/2020/bin/x86_64-linux"
+TEXLIVE_BIN="$TEXLIVE_DIR/2021/bin/x86_64-linux"
 
 # points to 2020 version, for some reason naming it 2020 doesn't work.
-INSTALL_TL="install-tl-unx.tar.gz"
-INSTALL_TL_VERSION="$(tar tf "$INSTALL_TL" | grep -om1 '^install-tl-[0-9]*')"
-INSTALL_TL_SUCCESS="$NETLIFY_CACHE_DIR/$INSTALL_TL_VERSION-success"
 
-TEXLIVEONFLY="$TEXLIVE_DIR/2020/texmf-dist/scripts/texliveonfly/texliveonfly.py"
+# INSTALL_TL="install-tl-unx.tar.gz"
+# INSTALL_TL_VERSION="$(tar tf "$INSTALL_TL" | grep -om1 '^install-tl-[0-9]*')"
+# INSTALL_TL_SUCCESS="$NETLIFY_CACHE_DIR/$INSTALL_TL_VERSION-success"
+
+# TEXLIVEONFLY="$TEXLIVE_DIR/2020/texmf-dist/scripts/texliveonfly/texliveonfly.py"
+
+# and actually 2020 version failed too, so let's try a build from current version of
+# https://github.com/frangio/netlify-latex/blob/master/build.sh
+
+
+INSTALL_TL_URL="http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz"
+INSTALL_TL="install-tl-unx.tar.gz"
+INSTALL_TL_SUCCESS="$NETLIFY_CACHE_DIR/install-tl-success"
+
+TEXLIVEONFLY="$TEXLIVE_DIR/2021/texmf-dist/scripts/texliveonfly/texliveonfly.py"
 
 # export CTAN_REPO="http://mirror.las.iastate.edu/tex-archive/systems/texlive/tlnet"
 
@@ -59,42 +70,54 @@ tlpdbopt_sys_bin /usr/local/bin
 tlpdbopt_sys_info /usr/local/share/info
 tlpdbopt_sys_man /usr/local/share/man
 tlpdbopt_w32_multi_user 1
-TEXDIR $TEXLIVE_DIR/2020
+TEXDIR $TEXLIVE_DIR/2021
 TEXMFLOCAL $TEXLIVE_DIR/texmf-local
-TEXMFSYSCONFIG $TEXLIVE_DIR/2020/texmf-config
-TEXMFSYSVAR $TEXLIVE_DIR/2020/texmf-var
+TEXMFSYSCONFIG $TEXLIVE_DIR/2021/texmf-config
+TEXMFSYSVAR $TEXLIVE_DIR/2021/texmf-var
 "
 
 echo "$TEXLIVE_PROFILE" > texlive.profile
 
-if [ ! -e "$INSTALL_TL_SUCCESS" ]; then
-  tar xf "$INSTALL_TL"
+# temporarily commenting out the condition in order that I can definitely get a fresh texlive install in.
 
+# if [ ! -e "$INSTALL_TL_SUCCESS" ]; then
   echo "[$0] Installing TeX Live..."
-  "$INSTALL_TL_VERSION"/install-tl --profile=texlive.profile
+
+  curl -L "$INSTALL_TL_URL" | tar xz --one-top-level=itl --strip-components=1
+  echo "$TEXLIVE_PROFILE" > texlive.profile
+  itl/install-tl --profile=texlive.profile
+  "$TEXLIVE_BIN/tlmgr" install latexmk texliveonfly
   echo "[$0] Installed TeX Live."
 
   touch "$INSTALL_TL_SUCCESS"
-else
-  echo "[$0] Found existing TeX Live installation."
-fi
+# else
+#   echo "[$0] Found existing TeX Live installation."
+# fi
+
 
 export PATH="$TEXLIVE_BIN:$PATH"
 
-# FREEZE 2020 repository
-tlmgr repository add ftp://tug.org/historic/systems/texlive/2020/tlnet-final
+# FREEZE 2020 repository (FAILING)
+#tlmgr repository add ftp://tug.org/historic/systems/texlive/2020/tlnet-final
+#
+#tlmgr option repository ftp://tug.org/historic/systems/texlive/2020/tlnet-final
 
-tlmgr option repository ftp://tug.org/historic/systems/texlive/2020/tlnet-final
+# NOTE TO SELF: to attempt to speed build, keeping track of packages that tlmgr says are already installed
+# when pinning to 2020 version:
+# xetex, l3packages, etoolbox, xcolor, microtype, l3kernel, regexpatch.  marvosym failed.
+# this may be because of netlify caching, and hence unreliable?? Not sure.  But I will try to comment them out and see if it still builds. 
 
-tlmgr --verify-repo=none update -self -all
-tlmgr --verify-repo=none install xetex
-tlmgr --verify-repo=none install l3packages
-tlmgr --verify-repo=none install etoolbox
-tlmgr --verify-repo=none install xcolor
-tlmgr --verify-repo=none install microtype
-tlmgr --verify-repo=none install l3kernel
-tlmgr --verify-repo=none install regexpatch
-tlmgr --verify-repo=none install marvosym
+# TEMPORARILY GETTING RID OF THIS TO SEE IF I CAN GET IT TO DO SOMETHING.
+
+# tlmgr --verify-repo=none update -self -all
+# tlmgr --verify-repo=none install xetex
+# tlmgr --verify-repo=none install l3packages
+# tlmgr --verify-repo=none install etoolbox
+# tlmgr --verify-repo=none install xcolor
+# tlmgr --verify-repo=none install microtype
+# tlmgr --verify-repo=none install l3kernel
+# tlmgr --verify-repo=none install regexpatch
+# tlmgr --verify-repo=none install marvosym
 
 python "$TEXLIVEONFLY" -c latexmk --compiler=xelatex -a "-g -pdf -synctex=1 -interaction=nonstopmode" "$@"
 
